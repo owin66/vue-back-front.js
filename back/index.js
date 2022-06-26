89d5a46c378bb24ee5c1ddeeeb658cb5b06d7e65
owin66 <78900270+owin66@users.noreply.github.com>
2022-06-26 13:15:57 +0300

    work_in_error

--- back/index.js
+++ back/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const http = require('http')
 
 //настрои подключение
+mongoose.connect(
+    'mongodb+srv://owin66:Pass321@nodejs.tyesk.mongodb.net/?retryWrites=true&w=majority',
+    {
+        userCreateIndex:true,
+        useNewUrlParser:true,
+    }
+)
 //инициализируем приложение
-
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 //объявим роуты
+const PORT = 3000
+http.createServer({}, app).listen(PORT)
+console.log(`Server running at ${PORT}`)
