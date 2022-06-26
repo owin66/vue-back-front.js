c84b70c12bd7cc2aa1df62e7102b8a8aa3c4925b
owin66 <78900270+owin66@users.noreply.github.com>
2022-06-26 09:46:02 +0300

    initial_project

---
+++ back/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+//настрои подключение
+//инициализируем приложение
+
+//объявим роуты
