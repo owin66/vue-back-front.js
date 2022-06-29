dfb2ec595e0ef6155145f8e5c7bc293f0715906c
owin66 <78900270+owin66@users.noreply.github.com>
2022-06-29 22:14:45 +0300

    refactor

--- back/index.js
+++ back/index.js
@@ -4,14 +4,17 @@ const mongoose = require('mongoose')
 const http = require('http')
 
 
+
 //настрои подключение
 mongoose.connect(
-    'mongodb://localhost:27017/stoic_allen',
+    'mongodb+srv://owin66:Pass321@nodejs.tyesk.mongodb.net/node-blog?retryWrites=true&w=majority',
 )
 //инициализируем приложение
+
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+
 //объявим роуты
 const PORT = 3000
 http.createServer({}, app).listen(PORT)
