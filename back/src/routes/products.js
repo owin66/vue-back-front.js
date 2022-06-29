8a6235c19844690a7aff34bf71316bba8bb8e05e
owin66 <78900270+owin66@users.noreply.github.com>
2022-06-29 23:29:40 +0300

    create_routes_category

--- back/src/routes/products.js
+++ back/src/routes/products.js
@@ -1,11 +1,11 @@
 const router = require('express-promise-router')
 
-const {category} = require('../controllers')
+const {product} = require('../controllers')
 
 router.route('/:id').get(product.get)
-router.route('/').get(product.get)
-router.route('/').get(product.get)
-router.route('/').get(product.get)
-router.route('/').get(product.get)
+router.route('/').post(product.create)
+router.route('/').get(product.getAll)
+router.route('/:id').put(product.update)
+router.route('/:id').delete(product.delete)
 
 module.exports = router
