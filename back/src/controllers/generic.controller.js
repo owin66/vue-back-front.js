47c7228f3d7f76e09b0c57907fc628a4cf97eed4
owin66 <78900270+owin66@users.noreply.github.com>
2022-06-29 23:02:50 +0300

    create_fool_controller

--- back/src/controllers/generic.controller.js
+++ back/src/controllers/generic.controller.js
@@ -3,35 +3,41 @@ const boom = require('boom')
 const genericCrud = (model) => ({
     async get({params: {id}}) {
         try {
-
+            const item = await model.findById(id)//vernet 1
+            return item
         } catch (err) {
             throw boom.boomify(err)
         }
     },
     async getAll() {
         try {
-
+            const item = await model.find(id)//vernet vse producti iz schema
+            return item
         } catch (err) {
             throw boom.boomify(err)
         }
     },
-    async create() {
+    async create({body}) { //title:Contrlo
         try {
-
+            const item = new model(body)//bodi iz frontend
+            const newItem = await item.save()//sohranit znacheniya
+            return newItem
         } catch (err) {
             throw boom.boomify(err)
         }
     },
-    async update() {
+    async update({params: {id}, body}) {//title Control
         try {
-
+            const item = await model.findByIdAndUpdate(id, body, {new: true})//update
+            return item
         } catch (err) {
             throw boom.boomify(err)
         }
     },
-    async delete() {
+    async delete({params: {id}}) {
         try {
-
+            await model.findByIdAndDelete(id)
+            return {status: 'OK', message: 'Продукт удален'}
         } catch (err) {
             throw boom.boomify(err)
         }
