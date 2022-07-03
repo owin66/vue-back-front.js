e6c149fb5aa7d650d78b2d651551efa9d23e60ea
owin66 <78900270+owin66@users.noreply.github.com>
2022-07-03 21:57:05 +0300

    refactoring_product2

--- front/src/store/product.js
+++ front/src/store/product.js
@@ -1,17 +1,46 @@
-const state = ()=>({
-    product:{},
-    products:[],
-    productError:null
+import {getProduct, getProducts} from "@/services/products.servise";
+
+const state = () => ({
+    product: {},
+    products: [],
+    productError: null
 })
 
-const mutations = {}
+const mutations = {
+    setProduct(state, product) {
+        state.product = product
+    },
+    setProducts(state, products) {
+        state.products = products
+    },
+    setProductError(state, error) {
+        state.productError = error
+    },
+}
 
-const actions = {}
+const actions = {
+    async fetchProduct({commit}, id) {
+        try {
+            const product = await getProduct(id)
+            commit('setProduct', product)
+        } catch (err) {
+            commit('setProductError', err)
+        }
+    },
+    async fetchProducts({commit}) {
+        try {
+            const products = await getProducts(id)
+            commit('setProducts', products)
+        } catch (err) {
+            commit('setProductError', err)
+        }
+    }
+}
 
 const getters = {
-    product:({ product})=>product,
-    products:({ products})=>products,
-    productError: null
+    product: ({product}) => product,
+    products: ({products}) => products,
+    productError: ({productError}) => productError
 }
 
 export default {
