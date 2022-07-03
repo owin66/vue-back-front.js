06066b5a0449d8e0abc5c99b6652d5a64e1d4c36
owin66 <78900270+owin66@users.noreply.github.com>
2022-07-04 00:52:44 +0300

    work_in_header.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <div id = 'app'>
+    <MevnHeader />
     <router-view/>
   </div>
 </template>
 
-
+<script>
+export default {
+  name: 'App',
+  components:{
+    MevnHeader: () => import('@/components/layouts/Header')
+  }
+}
+</script>
 
 
 <style lang="scss">
+body,
+html{
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- front/src/components/layouts/Header.vue
+++ front/src/components/layouts/Header.vue
@@ -4,7 +4,10 @@
       LOGO
     </div>
     <div class="header_right">
-
+      <ul class="nav">
+        <li class='nav_link' v-for="(category, key) in categories" :key="key"></li>
+<router-link>{{category.title}}</router-link>
+      </ul>
     </div>
 
   </header>
@@ -21,3 +24,12 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.header {
+  border-bottom: 1px solid;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
